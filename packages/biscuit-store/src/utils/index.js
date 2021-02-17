b44b7ff8da37d613ffffed29942cbb5dbc97c823
biscuit-js <biscuitstorejs@gmail.com>
2021-02-18 00:21:02 +0300

    update

--- packages/biscuit-store/src/utils/index.js
+++ packages/biscuit-store/src/utils/index.js
@@ -1,3 +1,3 @@
 export { createLog, CreateError, Warning } from '../internal/debugger';
-export { createEmmitor, emitter } from '../internal/emitter';
+export { emitter } from '../internal/emitter';
 export { throttle, debounce, sandbox } from '../internal/utils';
