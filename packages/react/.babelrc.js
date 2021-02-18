4902ae497fe848c029d3eda9e5805673160c1f0d
biscuit-js <biscuitstorejs@gmail.com>
2021-02-18 16:45:42 +0300

    update

--- packages/react/.babelrc.js
+++ packages/react/.babelrc.js
@@ -11,7 +11,7 @@ module.exports = {
         },
         exclude: ['transform-async-to-generator', 'transform-regenerator'],
         modules: false,
-        loose: true
+        loose,
       }
     ]
   ],
