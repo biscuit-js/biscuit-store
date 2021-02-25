d30dc4d4611fdd27bae7b00b0d200d7f57a71375
biscuit-js <biscuitstorejs@gmail.com>
2021-02-26 02:04:56 +0300

    update

--- packages/adapter/tests/action.test.js
+++ packages/adapter/tests/action.test.js
@@ -1,6 +1,27 @@
 /* eslint-disable sonarjs/no-duplicate-string */
 import { createAdapter } from '../src/index';
-import { testStore } from './testStore';
+import { createStore } from '../../biscuit-store';
+
+export const testStore = (name, adapter) => {
+    const { store, actions } = createStore({
+        repo: {
+            name,
+            initial: { value: 0 },
+        },
+        states: {
+            add: 'add/action',
+            remove: 'remove/action',
+        },
+        middleware: [adapter.connect],
+        strictMode: false,
+    });
+
+    return {
+        store,
+        add: actions.add,
+        remove: actions.remove,
+    };
+};
 
 it('calling action', (done) => {
     expect.assertions(2);
--- packages/adapter/tests/makeChannel.test.js
+++ packages/adapter/tests/makeChannel.test.js
@@ -1,5 +1,26 @@
 import { createAdapter } from '../src/index';
-import { testStore } from './testStore';
+import { createStore } from '../../biscuit-store';
+
+export const testStore = (name, adapter) => {
+    const { store, actions } = createStore({
+        repo: {
+            name,
+            initial: { value: 0 },
+        },
+        states: {
+            add: 'add/action',
+            remove: 'remove/action',
+        },
+        middleware: [adapter.connect],
+        strictMode: false,
+    });
+
+    return {
+        store,
+        add: actions.add,
+        remove: actions.remove,
+    };
+};
 
 it('calling action', (done) => {
     expect.assertions(2);
