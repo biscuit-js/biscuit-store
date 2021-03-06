8c1ffe4404013a68e75345a6fc9daecf26f8ae8e
biscuit-js <biscuitstorejs@gmail.com>
2021-03-07 01:38:36 +0300

    Update adapter.ts

--- playground/store/test/adapter.ts
+++ playground/store/test/adapter.ts
@@ -1,10 +1,11 @@
-import { createAdapter } from '../../../packages/adapter';
+// off prettier
+import {
+	createAdapter,
+} from '../../../packages/adapter';
 import { ITestStore } from '../../common/interfaces';
 import { BranchName, TestFetchPayload } from '../../common/types';
 
-const fetchFunc = async (
-	payload: TestFetchPayload
-): Promise<TestFetchPayload> => {
+const fetchFunc = async (payload: TestFetchPayload): Promise<TestFetchPayload> => {
 	return new Promise((resolve) => {
 		setTimeout(() => {
 			resolve(payload);
@@ -14,20 +15,12 @@ const fetchFunc = async (
 
 const adapter = createAdapter();
 
-adapter.action < ITestStore,
-	ITestStore,
-	ITestStore >
-		('test/add',
-		(payload, state, { getAction }) => {
-			getAction('test/step').dispatch <
-				BranchName >
-				{ name: 'prev: ' + state.value };
-			return { value: payload.value * 10 };
-		});
+adapter.action<ITestStore, ITestStore, ITestStore>('test/add', (payload, state, { getAction }) => {
+	getAction('test/step').dispatch<BranchName>({ name: 'prev: ' + state.value });
+	return { value: payload.value * 10 };
+});
 
-adapter.call < TestFetchPayload,
-	TestFetchPayload,
-	ITestStore > ('test/fetch', fetchFunc);
+adapter.call<TestFetchPayload, TestFetchPayload, ITestStore>('test/fetch', fetchFunc);
 
 const chan = adapter.makeChannel();
 
