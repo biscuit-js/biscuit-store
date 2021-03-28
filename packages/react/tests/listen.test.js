40ad93155f304d24213c667c5b53bc5261d46317
biscuit-js <biscuitstorejs@gmail.com>
2021-03-28 18:25:15 +0300

    Update listen.test.js

--- packages/react/tests/listen.test.js
+++ packages/react/tests/listen.test.js
@@ -16,7 +16,7 @@ afterEach(() => {
 	container = null;
 });
 
-it('render test', (done) => {
+it('listen.render test', (done) => {
 	expect.assertions(2);
 	const { add, store } = testStore('testrenderer');
 	let iteration = 0;
@@ -46,4 +46,87 @@ it('render test', (done) => {
 	});
 
 	add.dispatch({ state: true, value: 'test-sucсess' });
+});
+
+it('listen.update test', (done) => {
+	expect.assertions(3);
+	const { add, store } = testStore('testupdate', 'start');
+	let iteration = 0;
+	function TestComponent({ testupdate, field }) {
+		const { value } = testupdate;
+		useEffect(() => {
+			iteration += 1;
+
+			if (iteration === 1) {
+				expect(field).toBe('test-start-1');
+				expect(value).toBe('start');
+			}
+
+			if (iteration === 2) {
+				expect(value).toBe('updated');
+				done();
+			}
+		}, [value, field]);
+
+		return (
+			<div>
+				<p>{value}</p>
+			</div>
+		);
+	}
+
+	const RTestComponent = listen(store, { state: true }).update(TestComponent);
+
+	act(() => {
+		render(<RTestComponent field={'test-start-1'} />, container);
+	});
+	(async function () {
+		await add.dispatch({ state: false, value: 'no-updated' }).wait;
+		await add.dispatch({ state: true, value: 'updated' }).wait;
+	})();
+});
+
+it('listen.replace test', (done) => {
+	expect.assertions(4);
+	const { add, store } = testStore('testreplace', 'first');
+	const propText = 'test-first';
+
+	function TestComponent1({ testreplace, field }) {
+		const { value } = testreplace;
+		useEffect(() => {
+			expect(field).toBe(propText);
+			expect(value).toBe('first');
+		}, [value, field]);
+
+		return (
+			<div>
+				<p>{value}</p>
+			</div>
+		);
+	}
+
+	function TestComponent2({ testreplace, field }) {
+		const { value } = testreplace;
+		useEffect(() => {
+			expect(field).toBe(propText);
+			expect(value).toBe('last');
+			done();
+		}, []);
+
+		return (
+			<div>
+				<p>{value}</p>
+			</div>
+		);
+	}
+
+	const RTestComponent = listen(store, { value: 'last' }).replace(
+		TestComponent1,
+		TestComponent2
+	);
+
+	act(() => {
+		render(<RTestComponent field={propText} />, container);
+	});
+	add.dispatch({ value: 'last' });
 });
